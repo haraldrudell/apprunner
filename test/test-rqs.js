65a86ed6f63b04b7276c07d5752ed1b0308690b1
Harald Rudell <harald@therudells.com>
2012-09-15 04:20:49 -0700

    0.0.12 rqs updated

--- test/test-rqs.js
+++ test/test-rqs.js
@@ -1,4 +1,4 @@
-// test-rqs.js
+// test-rqs2.js
 // © Harald Rudell 2012
 
 var rqs = require('../lib/rqs')
@@ -8,98 +8,216 @@ var defTime = 100 // 100 ms
 var moreThanDefTime = defTime + 10 // 110 ms
 var longTime = 1e5 // 100 seconds
 
-exports['Rqs:'] = {
-	'Test': function (done) {
-		var cbResults = []
-		var logResults = []
-		var state = {
-			scope: 'rqFactory',
-			isDone: false,
-			count: 0,
-			cbCount: 0,
-			logCount: 0,
+var errors = []
+function errorListener(err) {
+	errors.push(err)
+}
+var filemarker = 'file: ' + __filename.substring(__filename.lastIndexOf('/') + 1)
+
+exports['Request Timer Factory:'] = {
+	'Plain Invocation': function () {
+		assert.equal(errors.length, 0, 'No errors before test')
+		var factory = rqs.getRqs()
+		assert.equal(typeof factory, 'object')
+		assert.equal(Object.keys(factory).length, 5)
+		Object.keys(factory).forEach(function (f) {
+			assert.equal(typeof factory[f], 'function', 'factory.' + f + ' should be function')
+		})
+		assert.equal(errors.length, 1, 'Verify Error of missing callback')
+
+		var e = errors[0]
+		assert.ok(~e.toString().indexOf(filemarker))
+		assert.ok(e.isTimer)
+		assert.ok(!e.isTimeout)
+		assert.ok(~e.scope.indexOf(filemarker))
+		assert.ok(~e.factoryFrom.indexOf(filemarker))
+		errors = []
+	},
+	'Default state': function () {
+		var aECbs = []
+		var factory = rqs.getRqs(errorCallback)
+
+		// verify we got a request
+		var before = Date.now()
+		var rq = factory.addRq()
+		var after = Date.now()
+		assert.ok(rq)
+		assert.ok(rq.id)
+
+		// verify the state object
+		var actual = factory.getState()
+		assert.ok(actual)
+		var rq = actual[rq.id]
+		assert.equal(typeof rq, 'object')
+		assert.equal(Object.keys(rq).length, 6)
+		assert.ok(rq.timer)
+		assert.equal(rq.timeout, 3000)
+		assert.ok(rq.created >= before)
+		assert.ok(rq.created <= after)
+		assert.ok(~rq.from.substring(filemarker))
+		assert.ok(~rq.param.substring(filemarker))
+
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
+
+		function errorCallback(err) {
+			aECbs.push(err)
 		}
-		var rq2Slogan = { a: 'a' }
+	},
+	'TimeoutMs': function () {
+		var aECbs = []
+		var time = 150
+		var factory = rqs.getRqs(errorCallback, undefined, time)
 
-		// instanitate
-		var rqFactory = rqs.getRqs(callback, state.scope, defTime, logFunc)
-		verifyState()
+		var rq = factory.addRq()
+		var state = factory.getState()
+		var actual = state[rq.id].timeout
+		assert.equal(actual, time)
 
-		// add a request with default 100 ms timeout
-		var rq1 = rqFactory.addRq()
-		assert.equal(typeof rq1, 'string')
-		assert.equal(rq1, '1')
-		state.count ++
-		verifyState()
-		setTimeout(rq1AfterTimeout, moreThanDefTime)
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
 
-		// add a request with long timeout
-		var rq2 = rqFactory.addRq(rq2Slogan, longTime)
-		assert.equal(typeof rq2, 'object')
-		assert.ok(rq2 != null)
-		assert.equal(rq2.rqId, '2')
-		state.count++
-		verifyState()
+		function errorCallback(err) {
+			aECbs.push(err)
+		}
+	},
+	'scopeName and .isTimeout()': function (done) {
+		var aECbs = []
+		var scope = 'SCOPE'
+		var time = 100
+		var moreTime = time + 1
+		var factory = rqs.getRqs(errorCallback, scope, time)
 
-		// clear rq2
-		var expected = false
-		var actual = rqFactory.clearRq(rq2)
-		assert.strictEqual(actual, false)
-		verifyState()
+		// scope only shows up in errors
+		var rq = factory.addRq()
+		assert.ok(!rq.isTimeout())
 
-		// clear illegal slogan
-		var actual = rqFactory.clearRq(17)
-		assert.strictEqual(actual, false)
-		state.cbCount++
-		var actualError = cbResults[cbResults.length - 1]
-		assert.ok(actualError instanceof Error)
-		assert.strictEqual(actualError.isTimeout, undefined)
-		verifyState()
+		setTimeout(next, moreTime)
 
-		// clear background
-		var actual = rqFactory.clearRq(0)
-		assert.strictEqual(actual, false)
-		verifyState()
+		function next() {
+			assert.ok(rq.isTimeout())
+			assert.equal(aECbs.length, 1)
+			assert.equal(errors.length, 1)
+			var e = errors[0]
+			errors = []
+			var actual = e.scope
+			assert.equal(actual, scope)
 
-		// when we get here, rq1 should have timed out
-		function rq1AfterTimeout() {
-
-			// verify correct number of error callbacks
-			state.cbCount++
-			if (cbResults.length != state.cbCount) {
-				console.log('callback problem:', cbResults)
-			}
-
-			// verify that we got a timeout error
-			var actualError = cbResults[cbResults.length - 1]
-			assert.ok(actualError instanceof Error)
-			assert.strictEqual(actualError.isTimeout, true)
-
-			verifyState()
-
-			var actual = rqFactory.clearRq(rq1)
-			assert.equal(typeof actual, 'number')
-			state.isDone = true
-			state.logCount++
-			verifyState()
+			factory.shutdown()
+			assert.ok(!errors.length)
 
 			done()
 		}
 
-		function verifyState() {
-			assert.equal(rqFactory.func, state.scope)
-			assert.equal(rqFactory.isDone(), state.isDone)
-			assert.equal(rqFactory.getCount(), state.count)
-			assert.equal(cbResults.length, state.cbCount)
-			assert.equal(logResults.length, state.logCount)
+		function errorCallback(err) {
+			aECbs.push(err)
 		}
+	},
+	'before': function () {
+		rqs.emitter.addListener('error', errorListener)
+	},
+	'after': function () {
+		rqs.emitter.removeListener('error', errorListener)
+	},
+}
 
-		function callback(err) {
-			cbResults.push(err)
-		}
+exports['Request Timer Instance:'] = {
+	'Default invocation and .toString()': function () {
+		var aECbs = []
+		var factory = rqs.getRqs(errorCallback)
 
-		function logFunc() {
-			logResults.push(arguments)
+		// verify we got a request
+		var before = Date.now()
+		var rq = factory.addRq()
+		var after = Date.now()
+		assert.equal(typeof rq, 'object')
+		assert.equal(Object.keys(rq).length, 7)
+		assert.equal(typeof rq.toString, 'function')
+		assert.equal(typeof rq.clear, 'function')
+		assert.equal(typeof rq.isTimeout, 'function')
+		assert.equal(typeof rq.id, 'string')
+		assert.ok(rq.created >= before)
+		assert.ok(rq.created <= after)
+		assert.ok(~rq.from.substring(filemarker))
+		assert.ok(~rq.param.substring(filemarker))
+
+		var s = rq.toString()
+		assert.ok(~s.indexOf('Request '))
+		assert.ok(~s.indexOf('created: ' + Date(rq.created)))
+		assert.ok(~s.indexOf('from: ' + rq.from))
+		assert.ok(~s.indexOf('parameter: '))
+		assert.ok(!~s.indexOf('timeout: '))
+
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
+
+		function errorCallback(err) {
+			aECbs.push(err)
 		}
+	},
+	'Parameter': function () {
+		var aECbs = []
+		var param = 'PARAM'
+		var factory = rqs.getRqs(errorCallback)
+
+		// verify we got a request
+		var rq = factory.addRq(param)
+		var actual = rq.param
+		assert.equal(actual, param)
+
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
+
+		function errorCallback(err) {
+			aECbs.push(err)
+		}
+	},
+	'Time': function () {
+		var aECbs = []
+		var time = 200
+		var factory = rqs.getRqs(errorCallback, undefined, time)
+
+		// verify we got a request
+		var rq = factory.addRq()
+		var state = factory.getState()
+		var data = state[rq.id]
+		var actual = data.timeout
+		assert.equal(actual, time)
+
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
+
+		function errorCallback(err) {
+			aECbs.push(err)
+		}
+	},
+	'.clear()': function () {
+		var aECbs = []
+		var factory = rqs.getRqs(errorCallback)
+
+		assert.ok(factory.isDone())
+		var rq = factory.addRq()
+		assert.ok(!factory.isDone())
+		rq.clear()
+		assert.ok(factory.isDone())
+
+		factory.shutdown()
+		assert.equal(aECbs.length, 0)
+		assert.ok(!errors.length)
+
+		function errorCallback(err) {
+			aECbs.push(err)
+		}
+	},
+	'before': function () {
+		rqs.emitter.addListener('error', errorListener)
+	},
+	'after': function () {
+		rqs.emitter.removeListener('error', errorListener)
 	},
 }
